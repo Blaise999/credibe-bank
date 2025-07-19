63e4aaef9cad2abd56dffdfaac25a00ee6b48b2c
Blaise Idoko <blaise999@example.com>
2025-07-19 22:26:36 +0100

    Added verifyToken middleware for settings route

--- New Backend/routes/settings.js
+++ New Backend/routes/settings.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { updateSettings } = require('../controllers/settings.controller');
-const verifyToken = require('../middleware/verifyToken');
+const { verifyToken } = require('../middleware/auth');
 
 router.post('/update', verifyToken, updateSettings);
 
