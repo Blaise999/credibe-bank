357c261e7a89d7fcae1d902c58cca4ce26b55a69
Blaise Idoko <blaise999@example.com>
2025-07-20 10:54:14 +0100

    🔧 Fix: admin routes handler function issue

--- New Backend/routes/admin.js
+++ New Backend/routes/admin.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const adminController = require("../controllers/admin.controller");
-const { injectFakeTransactions } = adminController; // 🧼 Removed cleanFakeTransactions
 const { verifyToken, isAdmin } = require("../middleware/auth");
 
 const Transaction = require("../models/transaction");
