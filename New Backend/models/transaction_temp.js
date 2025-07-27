7dcdca78d67fe64094c6d56edae4bcae0d4f8af5
Blaise Idoko <blaise999@example.com>
2025-07-27 10:01:55 +0100

    Temp rename Transaction.js to transaction_temp.js for Render case fix

---
+++ New Backend/models/transaction_temp.js
@@ -0,0 +1,54 @@
+// models/transaction.js
+const mongoose = require('mongoose');
+
+const transactionSchema = new mongoose.Schema(
+  {
+    from: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    to: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      default: null,
+    },
+    recipient: {
+      type: String,
+      required: true,
+    },
+    toIban: {
+      type: String,
+      required: true,
+    },
+    amount: {
+      type: Number,
+      required: true,
+    },
+    note: {
+      type: String,
+      default: 'Transfer',
+    },
+    type: {
+      type: String,
+      enum: ['credit', 'debit'],
+      default: 'debit',
+    },
+    status: {
+      type: String,
+      enum: ['pending', 'approved', 'rejected'],
+      default: 'pending',
+    },
+    date: {
+      type: Date,
+      default: Date.now,
+    },
+    receipt: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+// Check if model exists before defining
+module.exports = mongoose.models.Transaction || mongoose.model('Transaction', transactionSchema);
